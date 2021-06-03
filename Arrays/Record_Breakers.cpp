b554a6abbe29dff22d05d7fc81210c350bc27839
Ramavath Sairam <65453806+Sairam-04@users.noreply.github.com>
2021-06-03 23:54:04 +0530

    Add files via upload

---
+++ Arrays/Record_Breakers.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int ans=0;
+    int mx=INT16_MIN;
+    for(int i=0;i<n;i++){
+        if(arr[i]>mx && arr[i]>arr[i+1]){
+            ans++;
+        }
+        mx = max(mx,arr[i]);
+    }
+    cout<< ans<<endl;
+}
